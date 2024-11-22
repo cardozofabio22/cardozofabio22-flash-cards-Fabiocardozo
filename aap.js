9ab7407f828ba04d2c25dc9f8c5288c1dbcb04d6
cardozofabio22 <145573981+cardozofabio22@users.noreply.github.com>
2024-11-22 11:53:46 -0300

    22/11

--- aap.js
+++ aap.js
@@ -4,24 +4,24 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao';
 
     cartao.innerHTML = `
-                    <div class="cartao__conteudo">
-                    <h3>${categoria}</h3>
-                    <div class="cartao__conteudo__pergunta">
-                    <p>${pergunta}</p>
-                    </div>
-                    <div class="cartao__conteudo__resposta">
-                    <p>${pergunta}</p>
-                    </div>
-                    </div>`
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+    <p>${resposta}</p>
+    </div>
+    </div>
+    `
 
+    let respostaEstaVisivel = false;
 
-    let respostaestavisivel = false;
-
-    function viracartao() {
-        respostaestavisivel = !respostaestavisivel;
-        cartao.cllaslist.ttoggle('active', respostaestavisivel);
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
     }
-    cartao.addEventListener('click', viracartao)
+    cartao.addEventListener('click', viraCartao);
 
 
     container.appendChild(cartao);
